8442676a9b04688a6ccb88f35ad7365c9b92dd02
Matthew Harvey <software@matthewharvey.net>
2019-08-27 07:25:25 +1000

    Command line handling, build and binary execution tidy-up
    * Stop using oclif; don't need it
    * Rename "npm run prepack" to "npm run build"

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,11 @@
-import { Command, flags } from "@oclif/command";
-
 import { Board } from "./board";
 import { Card } from "./card";
 import { Deck } from "./deck";
 
-class Albertus extends Command {
-  public static description = "describe the command here";
-
-  public static flags = {
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: "f" }),
-    help: flags.help({ char: "h" }),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: "n", description: "name to print" }),
-    // add --version flag to show CLI version
-    version: flags.version({ char: "v" }),
-  };
-
-  public static args = [{ name: "file" }];
-
-  public async run() {
-    const deck = Deck.create().shuffle();
-    const board = Board.from(deck);
-    console.log(board.toString());
-  }
+async function main() {
+  const deck = Deck.create().shuffle();
+  const board = Board.from(deck);
+  console.log(board.toString());
 }
 
-export = Albertus;
+main();
