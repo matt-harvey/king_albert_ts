9245a7ab0e1931a5399d90e671c6c501777681f2
Matthew Harvey <software@matthewharvey.net>
2019-08-27 15:28:54 +1000

    Get user input and move cards accordingly

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,42 @@
+import * as readline from "readline";
+
 import { Board } from "./board";
 import { Card } from "./card";
 import { Deck } from "./deck";
+import { Move } from "./move";
 
 async function main() {
+  const clearScreen = "\x1b[2J\x1b[1;1H";
   const deck = Deck.create().shuffle();
-  const board = Board.from(deck);
+  let board = Board.from(deck);
+
+  console.log(clearScreen);
   console.log(board.toString());
+
+  const { stdin: input, stdout: output } = process;
+  const cli = readline.createInterface({ input, output });
+  cli.setPrompt("> ");
+  cli.prompt();
+  cli.on("line", input => {
+    const move = Move.from(input);
+    if (move === null) {
+      console.log(`Invalid move. Enter two letters, indicating the "from" position, followed by the "to" position".`);
+      cli.prompt();
+      return;
+    }
+    const newBoard = board.apply(move);
+    if (newBoard === null) {
+      console.log("Invalid move. Try again.");
+      cli.prompt();
+      return;
+    }
+    board = newBoard;
+    console.log(clearScreen);
+    console.log(board.toString());
+    cli.prompt();
+    return;
+  });
+
 }
 
 main();
