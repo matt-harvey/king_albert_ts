e69615625846207183a54dbdb24fd92e5d6129c7
Matthew Harvey <software@matthewharvey.net>
2019-08-30 14:33:09 +1000

    Improve documentation comments

--- src/game.ts
+++ src/game.ts
@@ -12,8 +12,15 @@ const helpText = `Enter two letters to describe your desired move, indicating th
 
 const clearScreen = "\x1b[2J\x1b[1;1H";
 
+// Immutably represents the state of a game at a given point in time. The Game contains a Board state, as well
+// as messages intended for display to the user. A Game has a string representation intended for
+// displaying in a terminal, representing that game state.
 export class Game {
 
+  // Creates a new game from the given board. Prompt is a string such as "> " that will be prepended
+  // when echoing user input. (The Game class is not actually responsible for input/output, but needs to
+  // know which prompt character to use to render a the complete string representation of the
+  // current game state.)
   public static from(board: Board, prompt: string): Game {
     return new Game(board, false, List<string>(), prompt);
   }
@@ -26,6 +33,7 @@ export class Game {
   ) {
   }
 
+  // Where command is user input, returns a new Game state that reflects that input.
   public apply(command: string): Game {
     const { board, prompt } = this;
     const promptedMessage = prompt + command;
